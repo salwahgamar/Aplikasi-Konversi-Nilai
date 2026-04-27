93964d72ef76cb337fd9bc3aa56955a336bdbe31
dhiahisanah <dhiahisanah@gmail.com>
2026-04-27 12:17:53 +0700

    pppppnavbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,6 @@ const Navbar = () => {
     <nav className="bg-blue-600 text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">Aplikasi Konversi Nilai & IPK</h1>
-        <div className="space-x-4">
-          <a href="#home" className="hover:underline">Home</a>
-        </div>
       </div>
     </nav>
   );
